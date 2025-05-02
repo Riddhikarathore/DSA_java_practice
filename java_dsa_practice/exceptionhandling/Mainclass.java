a508b12b82c7ac0c340ee26baa6f3b6629a9217e
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-02 23:32:40 +0530

    exceptionhandling

--- java_dsa_practice/exceptionhandling/Mainclass.java
+++ java_dsa_practice/exceptionhandling/Mainclass.java
@@ -1,5 +1,10 @@
 package exceptionhandling;
 
 public class Mainclass {
+public static void main (String args[]){
+    int a[]= new int [5];
 
+    System.out.println(a[8]);
+    System.out.println("good morning ");
+}
 }
