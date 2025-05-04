d39c9b54cfaa0246b9ff7c9638c7cb1ef4ead8a2
Riddhika rathore <riddhikarathore0101@gmail.com>
2025-05-05 00:02:53 +0530

    exception handling

--- java_dsa_practice/exceptionhandling/throwkeyword.java
+++ java_dsa_practice/exceptionhandling/throwkeyword.java
@@ -6,19 +6,30 @@ public class throwkeyword {
 
         Scanner sc = new Scanner (System.in);
         System.out.println("write age ");
-        int age = sc.nextInt();
+     //   int age = sc.nextInt();
 
         //ab let's suppose hum chahte h ki jiski age 100 se jyada ho waha execption aa jaye
         //toh ab hum yaha hum humara khudka execption de rahe h
         //throw ki help se
 
-        if(age>100){
-            throw new Exception();
+       // if(age>100){
+         //   throw new Exception();
             //ab ye yaha error dega kyuki ya toh apn ko ye try catch k andar likhna hoga
             //ya fir hum main method k sath throws krde exception ko
 
+// ab try ki help se krke dekhte h
 
+            try {
+                int age = sc.nextInt();
+                if (age > 100) {
+                    throw new Exception();
+                }
+            }
+                catch(Exception e){
+                    System.out.println("no allowed");
+
+            }
         }
 
     }
-}
+
